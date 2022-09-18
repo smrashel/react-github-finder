d25b692118ee8b4b038cb2e950bd3ea614be4caf
ppmspolsb <ppms.polsb@gmail.com>
2022-09-18 11:34:28 +0600

    Fixed GithubAction

--- src/components/users/User.js
+++ src/components/users/User.js
@@ -4,7 +4,7 @@ import {Link, useParams } from 'react-router-dom'
 import Spinner from '../layout/Spinner'
 import RepoList from '../repos/RepoList'
 import GithubContext from '../../context/github/GithubContext'
-import { getUser, getUserRepos } from '../../context/github/GithubActions'
+import { getUserAndRepos } from '../../context/github/GithubActions'
 
 function User() {
     const { user, loading, repos, dispatch } = useContext(GithubContext)
@@ -14,11 +14,8 @@ function User() {
     useEffect(() => {
         dispatch({type: 'SET_LOADING'})
         const getUserData = async() => {
-            const userData = await getUser(params.login)
-            dispatch({type: 'GET_USER', payload: userData})            
-            
-            const userRepoData = await getUserRepos(params.login)
-            dispatch({type: 'GET_REPOS', payload: userRepoData})
+            const userData = await getUserAndRepos(params.login)
+            dispatch({type: 'GET_USER_AND_REPOS', payload: userData})
         }
         getUserData()
     }, [])
